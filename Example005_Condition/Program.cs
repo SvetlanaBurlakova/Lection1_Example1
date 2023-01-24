bea38dd9b7cff0e8d92260415c91bf22f83548ce
Svetlana Burlakova <svetlya4ok83@gmail.com>
2023-01-24 22:24:06 +0400

    Example005_Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя");
+string username=Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Привет, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
