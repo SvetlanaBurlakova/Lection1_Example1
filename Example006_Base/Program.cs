ac8b64b15703533a3add9ff41e3f0f994fee4d48
Svetlana Burlakova <svetlya4ok83@gmail.com>
2023-01-25 12:13:52 +0400

    Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a=2;
+int b=8;
+int c=4;
+int d=3;
+int e=6;
+
+int max=a;
+
+if (b>max) max=b;
+if (c>max) max=c;
+if (d>max) max=d;
+if (e>max) max=e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
